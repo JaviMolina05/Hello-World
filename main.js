6fb7cb12a46f6de33dff19593186aec7a58fe51d
JaviMolina05 <fmolgal1705@g.educaand.es>
2024-10-09 20:37:40 +0200

    Ejercicio completo

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 import './style.css'
 
 
-document.querySelector('#app').innerHTML = `
-  
-`
+// Selecciona los elementos del DOM
+const account = document.getElementById('account');
+const buttons = document.querySelectorAll('.btn');
 
+// Inicializa el contador
+let count = 0;
 
+// Función para actualizar el valor mostrado del contador
+function updateCounter() {
+  account.textContent = count;
+}
+
+// Añadir eventos a cada botón
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    // Comprueba el texto del botón que ha sido clicado
+    if (this.textContent === 'Increase') {
+      count++;
+    } else if (this.textContent === 'Decrease') {
+      count--;
+    } else if (this.textContent === 'Reset') {
+      count = 0;
+    }
+    // Actualiza la pantalla con el nuevo valor
+    updateCounter();
+  });
+});
